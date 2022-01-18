721e29f92b127d5022162b38778fdab1d9479c46
Tyler887 <tylermageeshields@gmail.com>
2022-01-18 21:52:05 +0000

    Update installer.ps1

--- installer.ps1
+++ installer.ps1
@@ -2,5 +2,5 @@ cd $env:USERPROFILE\Downloads
 
 
 $latestVersion = 1.0
-$url = "https://github.com/psbuilder/psbuilder/releases/download/latest/PsBuilder.init.ps1"
-Invoke-WebRequest
+$url = "https://github.com/psbuilder/psbuilder/releases/download/$latestVersion/PsBuilder.init.ps1"
+Invoke-WebRequest $url
