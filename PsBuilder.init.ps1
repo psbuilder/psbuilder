d1f1b4550976d84d83c2fd685f715c77363478f1
Tyler887 <tylermageeshields@gmail.com>
2022-01-18 20:18:13 +0000

    Update PsBuilder.init.ps1

--- PsBuilder.init.ps1
+++ PsBuilder.init.ps1
@@ -1,27 +1,12 @@
+
 @echo off
+
 echo "PsBuilder - Version 0.1 Beta."
+
 echo "© 2022 PsBuilder and contributors. All rights reserved."
+
 echo "Use of this application is governed by a BSD license in the BSD3 file."
+
 echo ""
+
 echo "Build started."
-using (PowerShell ps = PowerShell.Create())
-
-{
-
-    ps
-
-        .AddScript(scriptString)
-
-        .AddCommand("Out-String");
-
-    var result = ps.Invoke();
-
-    foreach (var line in result)
-
-    {
-
-        Console.WriteLine(line);
-
-    }
-
-}
