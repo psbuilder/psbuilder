e612df011dacef21e30e59cf13feecc2b86b4a93
Tyler887 <tylermageeshields@gmail.com>
2022-01-18 21:52:38 +0000

    Update PsBuilder.init.ps1

--- PsBuilder.init.ps1
+++ PsBuilder.init.ps1
@@ -1,7 +1,6 @@
 
 
-echo "PsBuilder - Version 0.1 Beta"
-Write-Warning "For testing purposes only."
+echo "PsBuilder - Version 1.0 Stable"
 echo "© 2022 PsBuilder and contributors. All rights reserved."
 echo "Contains script modules in order to function properly!"
 echo "Use of this application is governed by a BSD license in the BSD3 file."
