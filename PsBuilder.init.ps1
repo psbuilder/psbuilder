53c41147ee4cd626753add9a42a4d68fdf21e54b
Tyler887 <tylermageeshields@gmail.com>
2022-01-18 20:29:24 +0000

    Update PsBuilder.init.ps1

--- PsBuilder.init.ps1
+++ PsBuilder.init.ps1
@@ -10,3 +10,26 @@ echo "Use of this application is governed by a BSD license in the BSD3 file."
 echo ""
 
 echo "Build started."
+$ErrorActionPreference = 'Stop'
+
+Set-Location -LiteralPath $PSScriptRoot
+
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
+
+$env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
+
+$env:DOTNET_NOLOGO = '1'
+
+dotnet tool restore
+
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+dotnet cake @args
+
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+  echo "Build complete successfully."
+else
+  Write-Error -Msg "Sorry! Your build failed."
+
+
+
