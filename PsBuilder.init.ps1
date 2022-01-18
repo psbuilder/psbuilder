c5db05e22b6fcd64efbeff51a4bb1a9bbbab6505
Tyler887 <tylermageeshields@gmail.com>
2022-01-18 21:11:07 +0000

    Update PsBuilder.init.ps1

--- PsBuilder.init.ps1
+++ PsBuilder.init.ps1
@@ -6,11 +6,11 @@ echo "© 2022 PsBuilder and contributors. All rights reserved."
 echo "Contains script modules in order to function properly!"
 echo "Use of this application is governed by a BSD license in the BSD3 file."
 
-Install-Module -Name ps2exe -Force
+Install-Module -Name PSBuilder
 
 echo ""
 echo "Build started."
-Invoke-ps2exe ".\PSApp.ps1" ".\WinApp.exe" -noConsole
+Invoke-Builder
 
 
 
