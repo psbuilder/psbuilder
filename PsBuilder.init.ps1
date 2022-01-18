994ff57ed2c8ee3ab8fa653d4c428d3183798d5e
Tyler887 <tylermageeshields@gmail.com>
2022-01-18 19:47:05 +0000

    Create PsBuilder.init.ps1

---
+++ PsBuilder.init.ps1
@@ -0,0 +1,6 @@
+@echo off
+echo "PsBuilder - Version 0.1 Beta."
+echo "© 2022 PsBuilder and contributors. All rights reserved."
+echo "Use of this application is governed by a BSD license in the BSD3 file."
+echo ""
+echo "Build started."
