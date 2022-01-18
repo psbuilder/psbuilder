a8282832a96d24aa59a13bea8ec9fbd131035779
Tyler887 <tylermageeshields@gmail.com>
2022-01-18 21:01:41 +0000

    Update PsBuilder.init.ps1

--- PsBuilder.init.ps1
+++ PsBuilder.init.ps1
@@ -6,7 +6,7 @@ echo "© 2022 PsBuilder and contributors. All rights reserved."
 echo "Contains script modules in order to function properly!"
 echo "Use of this application is governed by a BSD license in the BSD3 file."
 
-Install-Module -Name ps2exe
+Install-Module -Name ps2exe -Force
 
 echo ""
 echo "Build started."
