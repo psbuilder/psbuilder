d6ddcce5e15c84edce35f625bf64d2f20bf3ae29
Tyler887 <tylermageeshields@gmail.com>
2022-01-19 20:34:36 +0000

    Update PsBuilder.init.ps1

--- PsBuilder.init.ps1
+++ PsBuilder.init.ps1
@@ -5,12 +5,32 @@ echo "© 2022 PsBuilder and contributors. All rights reserved."
 echo "Contains script modules in order to function properly!"
 echo "Use of this application is governed by a BSD license in the BSD3 file."
 
-Install-Module -Name PSBuilder -Force
+try
 
-echo ""
-echo "Build started."
-Invoke-Builder
+{
 
+    while($true)
+
+    {
+
+        
+
+    
+
+      Install-Module -Name PSBuilder -Force
+
+      echo ""
+      echo "Build started."
+      Invoke-Builder
+      exit $LASTEXITCODE
+   }
+
+
+finally
+
+{
+  Write-Error "Exiting PsBuilder abnormally. Please manually clean anything up that was not finished!"
+}
 
 
 
