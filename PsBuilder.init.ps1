cfdf589ed71e388cc7c7d540f839ef4af4b6e7bc
Tyler887 <tylermageeshields@gmail.com>
2022-01-19 20:47:56 +0000

    Update PsBuilder.init.ps1

--- PsBuilder.init.ps1
+++ PsBuilder.init.ps1
@@ -4,33 +4,34 @@ echo "PsBuilder - Version 1.0 Stable"
 echo "© 2022 PsBuilder and contributors. All rights reserved."
 echo "Contains script modules in order to function properly!"
 echo "Use of this application is governed by a BSD license in the BSD3 file."
+# "PsBuilder" would be confused with "PSBuilder", the name of the module
+Install-Module -Name PSBuilder -Force
 
-try
+echo ""
+echo "Build started."
+Invoke-Builder
+exit $LASTEXITCODE
+   
 
-{
 
-    while($true)
 
-    {
+
+[Console]::TreatControlCAsInput = $true # at beginning of script
+
+    if ([Console]::KeyAvailable){
+
+        $readkey = [Console]::ReadKey($true)
+
+        if ($readkey.Modifiers -eq "Control" -and $readkey.Key -eq "C"){                
+
+            
+
 
         
 
     
-
-      Install-Module -Name PSBuilder -Force
-
-      echo ""
-      echo "Build started."
-      Invoke-Builder
-      exit $LASTEXITCODE
-   }
-}
-
-finally
-
-{
-  Write-Error "Exiting PsBuilder abnormally. Please manually clean anything up that was not finished!"
-}
+           Write-Error "You have been caught stopping the build process by pressing Control + C. Please clean anything up that PsBuilder did not finish."
+       }
 
 
 
