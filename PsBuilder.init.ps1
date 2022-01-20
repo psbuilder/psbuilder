a5f3d93b868ca23f42756003414afc96ee9817d7
Tyler887 <tylermageeshields@gmail.com>
2022-01-20 17:56:57 +0000

    Update PsBuilder.init.ps1

--- PsBuilder.init.ps1
+++ PsBuilder.init.ps1
@@ -13,12 +13,13 @@ echo "╚╝──╚══╩═══╩══╩╩═╩══╩══╩�
 echo "Version 1.1.0 Beta"
 echo "© 2022 PsBuilder and contributors. All rights reserved."
 echo "Contains script modules in order to function properly!"
-echo "Use of this application is governed by a BSD license in the BSD3 file."
+echo "Use of this application is governed by a BSD license in the LICENSE file."
+Write-Debug "PsBuilder is running on $Env:OS."
 # "PsBuilder" would be confused with "PSBuilder", the name of the module
 Install-Module -Name PSBuilder -Force
-Write-Debug "PsBuilder is running on $Env:OS."
+Write-Debug "Installed PsBuilder required module. Starting build."
 echo ""
-Write-Debug "Build started!"
+echo "Build started!"
 Invoke-Builder
 Write-Debug "Everything done"
    
