d3ec8a202c1dba1a53e31a20b0c4e9d99cde3ca5
Tyler887 <tylermageeshields@gmail.com>
2022-01-20 15:36:47 +0000

    Update PsBuilder.init.ps1

--- PsBuilder.init.ps1
+++ PsBuilder.init.ps1
@@ -1,6 +1,16 @@
 
+echo "╔═══╗──╔══╗────╔╗──╔╗"
 
-echo "PsBuilder - Version 1.0 Stable"
+echo "║╔═╗║──║╔╗║────║║──║║"
+
+echo "║╚═╝╠══╣╚╝╚╦╗╔╦╣║╔═╝╠══╦═╗"
+
+echo "║╔══╣══╣╔═╗║║║╠╣║║╔╗║║═╣╔╝"
+
+echo "║║──╠══║╚═╝║╚╝║║╚╣╚╝║║═╣║"
+
+echo "╚╝──╚══╩═══╩══╩╩═╩══╩══╩╝"
+echo "Version 1.1.0 Beta"
 echo "© 2022 PsBuilder and contributors. All rights reserved."
 echo "Contains script modules in order to function properly!"
 echo "Use of this application is governed by a BSD license in the BSD3 file."
